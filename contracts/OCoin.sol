829d41d1e4475c5e8b13dffe79c3b5a8b81365b3
oliver.meimberg <oliver.meimberg@form4.de>
2021-06-10 01:46:30 +0200

    doc

--- contracts/OCoin.sol
+++ contracts/OCoin.sol
@@ -15,62 +15,7 @@ pragma solidity ^0.4.24;
 // ----------------------------------------------------------------------------
 
 
-// ----------------------------------------------------------------------------
-// Lib: Safe Math
-// ----------------------------------------------------------------------------
-contract SafeMath {
 
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
-
-    function safeSub(uint a, uint b) public pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
-
-    function safeMul(uint a, uint b) public pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
-}
-
-
-/**
-ERC Token Standard #20 Interface
-https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-*/
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-
-    function transfer(address to, uint tokens) public returns (bool success);
-
-    function approve(address spender, uint tokens) public returns (bool success);
-
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
-
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
-
-
-/**
-Contract function to receive approval and execute function in one call
-Borrowed from MiniMeToken
-*/
-contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
-}
 
 /**
 ERC20 Token, with the addition of symbol, name and decimals and assisted token transfers
